c7420775ca44597bbe4f0eeda77e07cca56c29dd
Martin George <martingeorge191110@gmail.com>
2025-01-02 16:14:27 +0200

    Update state model

--- models/state.py
+++ models/state.py
@@ -3,30 +3,32 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    """DBStorage relationship"""
-    cities = relationship(
-        "City",
-        backref="state",
-        cascade="all, delete"
-    )
 
-    """FileStorage relationship"""
-    @property
-    def cities(self):
-        """Getter attribute to return the list of City instances"""
-        from models import storage
-        from models.city import City
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        """DBStorage relationship"""
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete"
+        )
+    else:
+        @property
+        def cities(self):
+            """Getter attribute to return the list of City instances"""
+            from models import storage
+            from models.city import City
 
-        all_cities = storage.all(City)
+            all_cities = storage.all(City)
 
-        return [
-            city for city in all_cities.values()
-            if city.state_id == self.id
+            return [
+                city for city in all_cities.values()
+                if city.state_id == self.id
             ]
