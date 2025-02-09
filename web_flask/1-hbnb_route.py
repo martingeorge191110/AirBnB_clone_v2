8f30aa08c8a160d550f5cc016148e9ae2d974f44
Martin George <martingeorge191110@gmail.com>
2025-02-09 13:12:18 +0200

    WEB FLASK (0, 1)

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that starts Flask application"""
+
+from flask import Flask
+
+server = Flask(__name__)
+
+
+@server.route("/", strict_slashes=False)
+def hello_hbnb():
+    """displays Hello HBNB! in / route"""
+    return ("Hello HBNB!")
+
+@server.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays HBNB in /hbnb route """
+    return "HBNB"
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
